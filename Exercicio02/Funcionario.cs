9ed35f4b2fd6dd02cb7ca0924eb08984ed99b8d1
JoaoVrf13 <joaovictorrfcontato@gmail.com>
2024-09-05 18:16:01 -0300

    Programa para aumento de salário de colaborador

---
+++ Exercicio02/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+
+namespace Exercicio02 {
+    internal class Funcionario {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+
+        public double Salarioliquido() {
+            return Salario - Imposto;
+        }
+
+        public void AumentoSalario(double porcentagem) {
+
+
+            Salario = Salario + (Salario * porcentagem / 100);
+
+
+        }
+        public override string ToString() {
+            return
+             Nome
+             + " , "
+             + "R$"
+             + Salarioliquido().ToString(CultureInfo.InvariantCulture);
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Funcionario F = new Funcionario();
+
+            Console.WriteLine("Nome: ");
+            F.Nome = Console.ReadLine();
+            Console.WriteLine("Salário bruto: ");
+            F.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Imposto");
+            F.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + F);
+
+            Console.WriteLine("Digite a porcentage, para aumentar o salário: ");
+            double aumento = double.Parse (Console.ReadLine());
+            F.AumentoSalario(aumento);
+
+            Console.WriteLine("Dados atualizados: " +F);
+
+
+
+
+        }
+    }
+}
